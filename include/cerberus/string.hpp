63e6437e3a660223bbb01d295e641024cd21793b
vaskozlov <vaskozlov3940@gmail.com>
2021-10-29 19:46:09 +0500

    change to header only library

--- include/cerberus/string.hpp
+++ include/cerberus/string.hpp
@@ -16,7 +16,35 @@ namespace cerb {
                                             sizeof(T) <= sizeof(uintmax_t) && x86_64;
     }
 
-    auto strlen(const char *__restrict str) -> size_t;
+    CERBLIB_DECL auto strlen(const char *__restrict str) -> size_t
+    {
+        size_t index = 0;
+
+#if defined(__x86_64__)
+        if (!std::is_constant_evaluated()) {
+            __asm__(
+                " mov $-16, %0                      \n"
+                " pxor %%xmm0, %%xmm0               \n"
+                "1:                                 \n"
+                " add $16, %0                       \n"
+                " pcmpistri $0x08, (%0,%1), %%xmm0  \n"
+                " jnz 1b                            \n"
+                " add %2, %0                        \n"
+                : "=a"(index)
+                : "d"(reinterpret_cast<size_t>(str)),
+                  "c"(reinterpret_cast<size_t>(str))
+                : "memory");
+            return index;
+        }
+#endif /* ARCH */
+
+        while (*str != '\0') {
+            str++;
+            index++;
+        }
+
+        return index;
+    }
 
     CERBLIB_CLANG_DISABLE_WARNING("-Wconstant-evaluated")
 
